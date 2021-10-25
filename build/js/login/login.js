68996dce4f2bd2e7bf93fa4a0aeabfe6a9e6a75a
marcusblanco <61409607+marcusblanco@users.noreply.github.com>
2021-10-25 09:04:16 +0400

    Testing gulp scripts

---
+++ build/js/login/login.js
@@ -0,0 +1,6 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+console.log("Login page with  es modules");
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImxvZ2luLmpzIl0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7O0FBQUFBLE9BQU8sQ0FBQ0MsR0FBUixDQUFZLDZCQUFaIiwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJMb2dpbiBwYWdlIHdpdGggIGVzIG1vZHVsZXNcIik7Il19
+},{}]},{},[1])
---
+++ src/scripts/login/login.js
@@ -0,0 +1,1 @@
+console.log("Login page with  es modules");
